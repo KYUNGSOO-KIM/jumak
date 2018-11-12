498df75ea7dd6bda49a4d1c6d83fcdf7fcfd31f9
unknown <djkks@nate.com>
2018-11-12 15:24:45 +0900

    20181112

--- DONGTAN/dongtanjumak/settings.py
+++ DONGTAN/dongtanjumak/settings.py
@@ -115,11 +115,16 @@ USE_L10N = True
 USE_TZ = True
 
 
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+)
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
+STATICFILES_DIRS=[os.path.join(BASE_DIR,'static'),]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
